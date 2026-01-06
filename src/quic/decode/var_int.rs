5e01854c38d53ce9eaeaf32be8d5e21347682811
Zack Kayser <zkayser@gmail.com>
2026-01-06 06:11:46 -0500

    Create VarInt module with public read function

---
+++ src/quic/decode/var_int.rs
@@ -0,0 +1,67 @@
+#![allow(dead_code)]
+use bytes::{Buf, Bytes};
+
+pub enum VarInt {
+    OneByte(u8),
+    TwoBytes(u16),
+    FourBytes(u32),
+    EightBytes(u64),
+}
+
+const MASK: u8 = 0b00111111;
+
+pub fn read(buf: &mut Bytes) -> Option<VarInt> {
+    let first_byte = buf.get_u8();
+    let prefix = first_byte >> 6;
+    let length = 1 << prefix;
+    match length {
+        1 => Some(VarInt::OneByte(first_byte & MASK)),
+        2 => {
+            if buf.remaining() < 1 {
+                return None;
+            }
+            let bytes: [u8; 2] = [first_byte & MASK, buf.get_u8()];
+            Some(VarInt::TwoBytes(u16::from_be_bytes(bytes)))
+        }
+        4 => {
+            if buf.remaining() < 3 {
+                return None;
+            }
+            let bytes: [u8; 4] = [first_byte & MASK, buf.get_u8(), buf.get_u8(), buf.get_u8()];
+            Some(VarInt::FourBytes(u32::from_be_bytes(bytes)))
+        }
+        8 => {
+            if buf.remaining() < 7 {
+                return None;
+            }
+            let bytes: [u8; 8] = [
+                first_byte & MASK,
+                buf.get_u8(),
+                buf.get_u8(),
+                buf.get_u8(),
+                buf.get_u8(),
+                buf.get_u8(),
+                buf.get_u8(),
+                buf.get_u8(),
+            ];
+            Some(VarInt::EightBytes(u64::from_be_bytes(bytes)))
+        }
+        _ => None,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bytes::Bytes;
+
+    #[test]
+    fn test_read_var_int_one_byte_length() {
+        let bytes = Bytes::from_static(&[0x25]);
+        if let Some(VarInt::OneByte(value)) = read(&mut bytes.clone()) {
+            assert_eq!(value, 37);
+        } else {
+            panic!("Expected OneByte variant");
+        };
+    }
+}
